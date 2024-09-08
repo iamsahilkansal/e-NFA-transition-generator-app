0a9428ecce1adbc7b1afb3526f42fb522cdb2b48
Sahil Kansal <kansalsahil51@gmail.com>
2024-09-09 02:59:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,7 @@ if __name__ == '__main__':
     table = []
     col1, col2=st.columns(2)
     with col1:
+        st.write("##")
         st.write("Enter the Regular Expression")
     with col2:
         regx=st.text_input("")
@@ -125,11 +126,11 @@ if __name__ == '__main__':
             for transition in table:
                 col1, col2, col3 = st.columns(3)
                 with col1:
-                    st.write(f"q[{transition['start']}]")
+                    st.text(f"q[{transition['start']}]")
                 with col2:
-                    st.write(f"q[{transition['final']}]")
+                    st.text(f"q[{transition['final']}]")
                 with col3:
-                    st.write(f"{transition['symbol']}")
+                    st.text(f"{transition['symbol']}")
             viewGraph(table)
         else:
-            st.write(f"**Enter a Valid Regular Expression**")
+            st.warning(f"**Enter a Valid Regular Expression**", icon="⚠️")
